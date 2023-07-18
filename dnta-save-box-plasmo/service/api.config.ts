28f016cea6a3da8536ee4e5d7f60bcdd367b55b1
jiaoyuhan <jyh1994@qq.com>
2023-07-18 10:44:01 +0800

    config

--- dnta-save-box-plasmo/service/api.config.ts
+++ dnta-save-box-plasmo/service/api.config.ts
@@ -1,4 +1,8 @@
+
 export const PAGE = 'http://127.0.0.1:7888';
-export const LOGIN_PAGE = 'http://127.0.0.1:7888/login';
 export const API_HOST = 'http://127.0.0.1:7052';
+// export const PAGE = 'http://dnta.rxdey.top';
+// export const API_HOST = `${PAGE}/apis`;
+
+export const LOGIN_PAGE = `${PAGE}/login`;
 export const BASE_URL = `${API_HOST}`;

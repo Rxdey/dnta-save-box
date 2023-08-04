c3d76144f9b32a52980627ef961a360c3e93ed43
jiaoyuhan <jyh1994@qq.com>
2023-08-04 10:06:48 +0800

    ✨ feat: 增加环境变量

--- dnta-save-box-plasmo/service/api.config.ts
+++ dnta-save-box-plasmo/service/api.config.ts
@@ -1,7 +1,7 @@
 // export const PAGE = 'http://127.0.0.1:7888';
 // export const API_HOST = 'http://127.0.0.1:7052';
-export const PAGE = '//dnta.rxdey.top';
-export const API_HOST = `${PAGE}/apis`;
+export const PAGE = process.env.PLASMO_PUBLIC_PAGE;
+export const API_HOST = process.env.PLASMO_API_HOST;
 
-export const LOGIN_PAGE = `https:${PAGE}/login`;
+export const LOGIN_PAGE = `${PAGE}/login`;
 export const BASE_URL = `${API_HOST}`;

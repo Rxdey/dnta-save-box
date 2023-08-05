39656be02459188336bd4c8b975e38ea5f8475da
rxdey <jyh1994@qq.com>
2023-08-05 12:22:58 +0800

    fix: PLASMO_PUBLIC

--- dnta-save-box-plasmo/service/api.config.ts
+++ dnta-save-box-plasmo/service/api.config.ts
@@ -1,7 +1,7 @@
 // export const PAGE = 'http://127.0.0.1:7888';
 // export const API_HOST = 'http://localhost:7052';
 export const PAGE = process.env.PLASMO_PUBLIC_PAGE;
-export const API_HOST = process.env.PLASMO_API_HOST;
+export const API_HOST = process.env.PLASMO_PUBLIC_API_HOST;
 
 export const LOGIN_PAGE = `${PAGE}/login`;
 export const BASE_URL = `${API_HOST}`;
